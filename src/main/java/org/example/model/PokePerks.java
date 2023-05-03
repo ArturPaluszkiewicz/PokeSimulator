1b0f2ce2e0b61e665ac14ac05e3f3ef0960b685a
Arturro <arturro_1996@o2.pl>
2023-05-03 18:55:14 +0200

    add some small changes in PokePerks class

--- src/main/java/org/example/model/PokePerks.java
+++ src/main/java/org/example/model/PokePerks.java
@@ -4,16 +4,28 @@ public class PokePerks {
     private boolean active;
     private Pokemon parent;
 
+    //Constructors
+    public PokePerks(Pokemon parent){
+        active = false;
+        this.parent = parent;
+    }
     public PokePerks(){
         active = false;
+        parent = null;
     }
 
+    //Methods
     public void activatePerks(){
         active = true;
     }
-
     public void deactivatePerks(){
         active = false;
+    }
+    private boolean checkIfParentSet(){
+        if(!(parent == null)){
+            return true;
+        }
+        return false;
     }
 
 }
